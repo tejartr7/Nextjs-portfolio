b897994aafa9bdb7860fed80996c6c18b1371f2a
tejartr7 <raghusai126@gmail.com>
2023-12-17 08:58:47 +0530

    contact commit

--- app/contact/page.jsx
+++ app/contact/page.jsx
@@ -1,14 +1,29 @@
-import React from 'react'
+import React from 'react';
+import { IoIosMail } from 'react-icons/io';
+import { FaXTwitter } from "react-icons/fa6";
+import Icon from '../techstack/Icon';
 
 const Contact = () => {
     return (
-        <section id='contact'>
-            <div className='gap-4 mx-4 lg:mx-40 mt-6 mb-4 lg:mb-8' style={{ borderTop: '5px solid #ffff00', color: 'white' }}>
-                <h1 className='text-center text-4xl m-2' style={{color:"#ffff00"}}>Contact me</h1>
-                
+        <section id="contact">
+            <div className="flex flex-col items-center gap-4 mx-4 lg:mx-40 mt-6 mb-8 text-white" style={{ borderTop: '5px solid #ffff00', color: 'white' }}>
+                <h1 className="text-4xl m-2" style={{ color: '#ffff00' }}>Contact me</h1>
+                <div className="flex gap-4">
+                    <a className="text-yellow-500" href="mailto:codworldrtr7@gmail.com" target='_blank'>
+                        <Icon>
+                            <IoIosMail />
+                        </Icon>
+                    </a>
+                    <a className="text-yellow-500" href='https://twitter.com/raghu_rtr' target='_blank'>
+                        <Icon>
+                            <FaXTwitter />
+                        </Icon>
+                    </a>
+                </div>
+                <p className="text-500" style={{ color: '#ffff00' }}>&#169; copyright-RTR</p>
             </div>
         </section>
-    )
-}
+    );
+};
 
-export default Contact
+export default Contact;
--- app/projects/page.jsx
+++ app/projects/page.jsx
@@ -4,7 +4,7 @@ const Projects = () => {
     return (
         <section id="projects">
             <div className='gap-4 mx-4 lg:mx-40 mt-6 mb-4 lg:mb-8' style={{ borderTop: '5px solid #ffff00', color: 'white' }}>
-                <h1>Projects</h1>
+            <h1 className="text-4xl m-2 text-center" style={{ color: '#ffff00' }}>Projects</h1>
             </div>
         </section>
     )
