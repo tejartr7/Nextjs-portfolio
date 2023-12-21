d00b801c5b51a236e628497a5ee4f3dec36e4dce
tejartr7 <raghusai126@gmail.com>
2023-12-21 12:23:05 +0530

    logo commit

--- app/layout.js
+++ app/layout.js
@@ -11,6 +11,9 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+        <link type='icon' href='/favicon.ico' rel='icon' />
+      </head>
       <body className={inter.className}>{children}</body>
     </html>
   )
