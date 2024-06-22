0863a2c30aac49d3e8b92c37914010cc63d14bfd
RTR 🚀 <97605400+tejartr7@users.noreply.github.com>
2024-06-22 13:25:39 +0530

    added clarity for analytics

--- app/layout.js
+++ app/layout.js
@@ -1,32 +1,47 @@
-import { Inter } from 'next/font/google';
-import './globals.css';
-
-const inter = Inter({ subsets: ['latin'] });
+import { Inter } from "next/font/google";
+import "./globals.css";
+import Script from "next/script";
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: 'Rtr Portfolio',
-  description: 'Created by RTR',
+  title: "Rtr Portfolio",
+  description: "Created by RTR",
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <head>
-        <script type="text/javascript">
-          {`
-            (function(c,l,a,r,i,t,y){
-              c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
-              t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
-              y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
-            })(window, document, "clarity", "script", "msxmxt1reu");
-          `}
-        </script>
-        <script async defer src="https://scripts.simpleanalyticscdn.com/latest.js"></script>
+        <Script
+          id="clarity-script"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `(function(c,l,a,r,i,t,y){
+          c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+          t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+          y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+      })(window, document, "clarity", "script", "msxmxt1reu");`,
+          }}
+        />
+        <Script
+          id="clarity-script"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `(function(c,l,a,r,i,t,y){
+            c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+            t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+            y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+          })(window, document, "clarity", "script", "msxmxt1reu");`,
+          }}
+        />
+        <script
+          async
+          defer
+          src="https://scripts.simpleanalyticscdn.com/latest.js"
+        ></script>
         <link rel="icon" href="/favicon.ico" type="image/x-icon" />
       </head>
-      <body className={inter.className}>
-        {children}
-      </body>
+      <body className={inter.className}>{children}</body>
     </html>
   );
 }
